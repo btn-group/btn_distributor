56b75bbe44e4e1c86cfdcee952f8c94396dc5c4b
Steven C <steven.chang@btn.group>
2023-06-06 19:24:18 +1000

    Add Openbrush and query config (#1)

--- lib.rs
+++ lib.rs
@@ -2,68 +2,35 @@
 
 #[ink::contract]
 mod btn_distributor {
+    use openbrush::{contracts::ownable::*, traits::Storage};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    // === STRUCTS ===
+    #[derive(Debug, Clone, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct Config {
+        admin: AccountId,
+    }
+
     #[ink(storage)]
+    #[derive(Default, Storage)]
     pub struct BtnDistributor {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        #[storage_field]
+        ownable: ownable::Data,
     }
 
     impl BtnDistributor {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            let mut instance = Self::default();
+            instance._init_with_owner(Self::env().caller());
+            instance
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let btn_distributor = BtnDistributor::default();
-            assert_eq!(btn_distributor.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut btn_distributor = BtnDistributor::new(false);
-            assert_eq!(btn_distributor.get(), false);
-            btn_distributor.flip();
-            assert_eq!(btn_distributor.get(), true);
+        pub fn config(&self) -> Config {
+            Config {
+                admin: self.ownable.owner(),
+            }
         }
     }
 }
